fc14199c6cd7fa1eb6a779af5cb9212136d8dfc8
Abhiraj Vishwakarma <abhirajv079@gmail.com>
2026-05-02 00:03:28 +0530

    add role in users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            // Role column for Admin and Employee
+            $table->enum('role', ['admin', 'employee'])->default('employee');
+            
             $table->rememberToken();
             $table->timestamps();
         });
