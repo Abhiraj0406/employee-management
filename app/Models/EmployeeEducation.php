b244ef7af317da1175e6ea5c1c87c2e12c1dfd76
Abhiraj Vishwakarma <abhirajv079@gmail.com>
2026-05-02 01:08:43 +0530

    Add relationships and fillable attributes to EmployeeProfile and EmployeeEducation models

--- app/Models/EmployeeEducation.php
+++ app/Models/EmployeeEducation.php
@@ -1,10 +1,20 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class EmployeeEducation extends Model
 {
-    //
+    protected $fillable = [
+        'employee_profile_id',
+        'certificate_name',
+        'institute_name',
+        'year_of_completion',
+        'document_file',
+    ];
+
+    public function employeeProfile()
+    {
+        return $this->belongsTo(EmployeeProfile::class);
+    }
 }
